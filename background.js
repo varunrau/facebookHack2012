9eb2893e8035cbd65a2eaf228c52f857e23dd167
Varun Rau <varunrau@gmail.com>
2012-10-13 01:12:59 -0700

    sending proper requests

--- background.js
+++ background.js
@@ -1,28 +1,42 @@
 alert("loaded");
 
-function sendURLtoServer(url) {
-	var serverURL = "http://blooming-falls-6379.herokuapp.com/"; 
-	var http = new XMLHttpRequest();
-	http.open("PUT", serverURL, true);
-	http.send(url);
-	http.onreadystatechange = function() {
-		if(http.readyState == 4) {
-			alert("sent correctly");
-		}
-	}
+var pastUrls = new Array();
+
+function sendURLtoServer(tab) {
+	var serverURL = "http://blooming-falls-6379.herokuapp.com/from_links/link"; 
+	//var http = new XMLHttpRequest();
 	
+	//http.open("PUT", serverURL, true);
+	var json = {
+		"from_link[url]" : pastUrls[tab.index], 
+		"from_link[to_link]": tab.url
+		};
+	alert("sending to server");
+	$.get("http://blooming-falls-6379.herokuapp.com/from_links/link", json, 
+	function(data) {
+		alert(data);
+	});
 };
 
+chrome.tabs.onCreated.addListener(function(tab) {
+	pastUrls[tab.index] = tab.url;
+	if (tab.url !== undefined && tab.status == 'complete') {
+		alert("fafd");
+		sendURLtoServer(tab);
+	}
+	alert("hi");
+});
 
-// Event Listener for new tabs
-// chrome.tabs.onCreated.addListener(function() {
-//     alert("entered Lisenter");
-// 	sendURLtoServer(tab.url);
-// 	alert("ended listener");
-// });
+chrome.tabs.onRemoved.addListener(function(tab) {
+	var curIndex = tab.index;
+	pastUrls.splice(curIndex, curIndex + 1);
+});
 
 chrome.tabs.onUpdated.addListener(function(integer, changeInfo, tab) {
-	if (tab.status == 'complete') {
-		sendURLtoServer(tab.url);
+	console.write("updated");
+	alert("hello");
+	if (tab.url !== undefined && tab.status == 'complete') {
+		alert("hel");
+		sendURLtoServer(tab);
 	}
 });
