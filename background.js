5aae15f7f9a9a97597c1b341c0adf998c159ca34
Varun Rau <varunrau@gmail.com>
2012-10-12 22:18:30 -0700

    pinged server correctly

---
+++ background.js
@@ -0,0 +1,28 @@
+alert("loaded");
+
+function sendURLtoServer(url) {
+	var serverURL = "http://blooming-falls-6379.herokuapp.com/"; 
+	var http = new XMLHttpRequest();
+	http.open("PUT", serverURL, true);
+	http.send(url);
+	http.onreadystatechange = function() {
+		if(http.readyState == 4) {
+			alert("sent correctly");
+		}
+	}
+	
+};
+
+
+// Event Listener for new tabs
+// chrome.tabs.onCreated.addListener(function() {
+//     alert("entered Lisenter");
+// 	sendURLtoServer(tab.url);
+// 	alert("ended listener");
+// });
+
+chrome.tabs.onUpdated.addListener(function(integer, changeInfo, tab) {
+	if (tab.status == 'complete') {
+		sendURLtoServer(tab.url);
+	}
+});
